f9abbc388c7a0e2ac689716807838f8adeb3c40f
Martin Tabakov <MHTabakov@gmail.com>
2023-01-07 18:55:46 +0200

    Add  Item Create & Edit forms validators

--- Web Course Wrork FE/web-cw/src/app/inventory/item-edit/item-edit.component.ts
+++ Web Course Wrork FE/web-cw/src/app/inventory/item-edit/item-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Console } from 'console';
 import { switchMap } from 'rxjs/internal/operators/switchMap';
 import { item } from 'src/app/data/models/item';
 import { InventoryService } from 'src/app/services/inventory.service';
@@ -19,17 +21,18 @@ export class ItemEditComponent implements OnInit {
     private formBuilder: FormBuilder,
     private inventoryService: InventoryService,
     private router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private failedUpdate: MatSnackBar) {
 
 
     this.formGroup = this.formBuilder.group({
-      name: ["", Validators.required],
+      name: ["", [Validators.required, Validators.maxLength(50)]],
       category: ["", Validators.required],
-      purchasePrice: ["", Validators.required],
-      retailPrice: ["", Validators.required],
-      quantity: ["", Validators.required],
+      purchasePrice: ["", [Validators.required, Validators.min(0.01)]],
+      retailPrice: ["", [Validators.required, Validators.min(0.01)]],
+      quantity: ["", [Validators.required, Validators.min(0)]],
       code: ["", Validators.required],
-      description: ["", Validators.required]
+      description: ["", [Validators.required, Validators.maxLength(2000)]]
     });
   }
 
@@ -40,6 +43,7 @@ export class ItemEditComponent implements OnInit {
         let id = String(params.get('id'));
         return this.inventoryService.getById(id);
       })).subscribe(res => {
+
         this.item = res;
         this.formGroup.patchValue({
           name: this.item.name,
@@ -56,7 +60,13 @@ export class ItemEditComponent implements OnInit {
   onSubmit(): void {
     let values = this.formGroup.value;
     if (this.item) {
-      this.inventoryService.update(values, this.item.id).subscribe(next => this.router.navigate(['details', this.item?.id]));
+      this.inventoryService.update(values, this.item.id).subscribe(next => {
+          this.router.navigate(['details', this.item?.id])
+      },
+      error => {
+        console.log(error);
+        this.failedUpdate.open("Failed to update item.", "Close")
+      });
     }
 
   }
--- Web Course Wrork FE/web-cw/src/app/inventory/item-new/item-new.component.ts
+++ Web Course Wrork FE/web-cw/src/app/inventory/item-new/item-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/internal/operators/switchMap';
 import { item } from 'src/app/data/models/item';
@@ -19,27 +20,33 @@ export class ItemNewComponent implements OnInit {
     private formBuilder: FormBuilder,
     private inventoryService: InventoryService,
     private router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private failedCreateItem: MatSnackBar) {
 
 
     this.formGroup = this.formBuilder.group({
-      name: ["", Validators.required],
+      name: ["", [Validators.required, Validators.maxLength(50)]],
       category: ["", Validators.required],
-      purchasePrice: ["", Validators.required],
-      retailPrice: ["", Validators.required],
-      quantity: ["", Validators.required],
+      purchasePrice: ["", [Validators.required, Validators.min(0.01)]],
+      retailPrice: ["", [Validators.required, Validators.min(0.01)]],
+      quantity: ["", [Validators.required, Validators.min(0)]],
       code: ["", Validators.required],
-      description: ["", Validators.required]
+      description: ["", [Validators.required, Validators.maxLength(2000)]]
     });
   }
 
-  ngOnInit(): void {  }
+  ngOnInit(): void { }
 
 
   onSubmit(): void {
     let values = this.formGroup.value;
 
-    this.inventoryService.create(values).subscribe( next=> this.router.navigate(['details',next.id]));
+    this.inventoryService.create(values).subscribe(
+      next => this.router.navigate(['details', next.id]),
+      error => {
+        console.log(error);
+        this.failedCreateItem.open("Failed to create item.", "Close")
+      });
   }
 
   onBack(): void {
