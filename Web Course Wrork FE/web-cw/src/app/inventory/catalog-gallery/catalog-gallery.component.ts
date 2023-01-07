4f91389df45b53ddb5644d2ed42c560dd9722f86
Martin Tabakov <MHTabakov@gmail.com>
2023-01-07 19:27:16 +0200

    Add item catalog pagination

--- Web Course Wrork FE/web-cw/src/app/inventory/catalog-gallery/catalog-gallery.component.ts
+++ Web Course Wrork FE/web-cw/src/app/inventory/catalog-gallery/catalog-gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { catalog_item } from 'src/app/data/interfaces/catalog_item';
 import { InventoryService } from 'src/app/services/inventory.service';
 
@@ -9,14 +10,43 @@ import { InventoryService } from 'src/app/services/inventory.service';
 })
 export class CatalogGalleryComponent implements OnInit {
 
+  length = 50;
+  pageSize = 2;
+  pageIndex = 0;
+
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent: PageEvent | undefined;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    this.setPaginatedData();
+  }
+
   dataSource: catalog_item[] = [];
+  paginatedData: catalog_item[] = [];
 
   displayedColumns = ["name","category","price","view"];
 
   constructor(private inventoryService:InventoryService) { }
 
   ngOnInit(): void {
-    this.inventoryService.getAll().subscribe(next => this.dataSource = next);
+    this.inventoryService.getAll().subscribe(next => {
+       this.dataSource = next;
+       this.length = this.dataSource.length;
+       this.setPaginatedData();
+      });
+  }
+
+  private setPaginatedData(): void {
+    let start = this.pageSize * this.pageIndex;
+    let end: number | undefined = this.pageSize * this.pageIndex + this.pageSize;
+    if(end > this.dataSource.length) end = undefined;
+    this.paginatedData = this.dataSource.slice(start, end);
   }
 
 }
